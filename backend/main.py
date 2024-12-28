1d572f711a6da63bf49b586d740cb3af05804c4c
veenzent <odumevincent19@gmail.com>
2024-12-28 23:41:58 +0100

    Removed allowed origins wildcard

--- backend/main.py
+++ backend/main.py
@@ -35,15 +35,16 @@ app = FastAPI(
 )
 
 origins = [
-    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+    "https://boredtap.netlify.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    # allow_origins=origins,
+    # allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    # allow_methods=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
