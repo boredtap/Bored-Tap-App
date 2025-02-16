78a16249ed5367675882a9931d5101e73b2706a2
veenzent <odumevincent19@gmail.com>
2025-02-16 16:02:20 +0100

    WebApp can now be launched from telegram inline keyboard button

--- backend/telegram_bot.py
+++ backend/telegram_bot.py
@@ -2,13 +2,11 @@ import time
 import requests
 import telebot
 import asyncio
-from telebot.types import Message, Update
+from telebot.types import Message, Update, InlineKeyboardMarkup, InlineKeyboardButton
 from config import get_settings
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from user_reg_and_prof_mngmnt.router import sign_up
-from user_reg_and_prof_mngmnt.dependencies import get_user_by_id, insert_new_invite_ref, insert_new_user
 from user_reg_and_prof_mngmnt.schemas import Signup
-from user_reg_and_prof_mngmnt.user_authentication import create_invite_ref, create_invited_user, reward_inviter_and_invitee, validate_referral_code
 
 
 bot_interactions = APIRouter()
@@ -18,8 +16,7 @@ bot = telebot.TeleBot(token=BotToken)
 base_url = get_settings().base_url
 
 
-
-# set webhook url
+# --------------------------------- Set Webhook URL ----------------------------------- #
 def set_webhook_url():
     webhook_url = f"{base_url}/webhook"
     # webhook_url = "https://d5be-197-149-69-222.ngrok-free.app/webhook"
@@ -34,15 +31,23 @@ def set_webhook_url():
 
 set_webhook_url()
 
+
+# ---------------------------------- Webhook Handler ----------------------------------- #
 # process incoming Updates from telegram
 @bot_interactions.post('/webhook', tags=["Registration/Authentication"])
 async def webhook_handler(request: Request):
+    print("Received webhook request")
     # parse request body as json
     json_string: Update = await request.json()
 
     # Process the update here
     update = Update.de_json(json_string)
     bot.process_new_updates([update])
+
+    return {
+        "message": "Webhook request processed successfully",
+        "status": "ok"
+    }
 
 # get user profile picture
 def get_profile_url(user_id: int):
@@ -56,13 +61,19 @@ def get_profile_url(user_id: int):
 @bot.message_handler(commands=['start'])
 def start_command(message: Message):
     # get user datails
-    user_id = str(message.from_user.id)
-    referral_code = message.text.split(" ")[1]
-    username = message.from_user.username
-    first_name = message.from_user.first_name
-    last_name = message.from_user.last_name
-    image_url = get_profile_url(user_id)
+    try:
+        user_id = str(message.from_user.id)
+        # referral_code = message.text.split(" ")[1]
+        referral_code = "1234521345"
+        username = message.from_user.username
+        first_name = message.from_user.first_name
+        last_name = message.from_user.last_name
+        image_url = get_profile_url(user_id)
+    except Exception as e:
+        print(e)
+        # raise e
 
+    # instantiate Signup model with user details
     print("Setting user details in Signup model")
     user = Signup(
         telegram_user_id=user_id,
@@ -72,61 +83,47 @@ def start_command(message: Message):
     
     print("calling sign_up function")
     try:
+        # run sign_up function
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         referred_user = loop.run_until_complete(sign_up(user, referral_code))
         loop.close()
 
-        bot.send_message(
-            message.chat.id, f"Welcome, {referred_user.username}!\nClick the button below to launch app: ",
-            reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True).
-                add(telebot.types.InlineKeyboardButton("Launch:", url="https://boredtap.netlify.app"))
+        # send welcome message
+        launch_btn = InlineKeyboardButton(
+            text="Launch WebApp",
+            # callback_data="launch_webapp"
+            web_app=telebot.types.WebAppInfo(url="https://boredtap.netlify.app/")
         )
+        inline_keyboard = InlineKeyboardMarkup(row_width=1).add(launch_btn)
+        bot.send_message(
+            message.chat.id, f"Welcome, {referred_user.username}!\nPerform tasks and earn coins!",
+            reply_markup=inline_keyboard
+        )
+
+    # except user already exist
+    except HTTPException as e:
+        # send welcome message
+        launch_btn = InlineKeyboardButton(
+            text="Launch WebApp",
+            # callback_data="launch_webapp",
+            web_app=telebot.types.WebAppInfo(url="https://boredtap.netlify.app/")
+        )
+        inline_keyboard = InlineKeyboardMarkup(row_width=1).add(launch_btn)
+        bot.send_message(
+            message.chat.id, f"Welcome back, {username}!\nPerform tasks and earn coins!",
+            reply_markup=inline_keyboard
+        )
+
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
+
+    print("Start command handled")
 
 
-
-    # # check if invited user already exists and sign them in
-    # existing_user = get_user_by_id(user_id)
-    # if existing_user:
-    #     # create an inline keyboard with a URL button
-    #     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    #     launch_btn = telebot.types.InlineKeyboardButton("Launch:", url="https://boredtap.netlify.app")
-    #     keyboard.add(launch_btn)
-
-    #     bot.send_message(
-    #         message.chat.id, f"Welcome, {username}!\nClick the button below to launch app: ",
-    #         reply_markup=keyboard
-    #     )
-
-    # # else register them
-    # user = Signup(
-    #     telegram_user_id=user_id,
-    #     username=username,
-    #     image_url=image_url
-    # )
-
-    # if referral_code and validate_referral_code(str(referral_code)):
-    #     invited_user = create_invited_user(invited=user)
-    #     new_invite_ref = create_invite_ref(inviter_id=referral_code, ref=user)
-    #     insert_new_user(invited_user)
-
-    #     if new_invite_ref:
-    #         insert_new_invite_ref(new_invite_ref)
-    #     reward_inviter_and_invitee(inviter_id=referral_code, invitee_id=user_id, reward=100)
-
-    #     # create an inline keyboard with a URL button
-    #     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    #     launch_btn = telebot.types.InlineKeyboardButton("Launch:", url="https://boredtap.netlify.app")
-    #     keyboard.add(launch_btn)
-
-    #     # send welcome message
-    #     bot.send_message(
-    #         message.chat.id, f"Welcome, {username}!\nClick the button below to launch app",
-    #         reply_markup=keyboard
-    #     )
-
-    print("Command handled")
-
+# launch webapp button
+# @bot.callback_query_handler(func=lambda call: True)
+# def launch_webapp(call):
+#     if call.data == "launch_webapp":
+#         bot.answer_callback_query(call.id, url="https://veenzent.netlify.app/", show_alert=True)
 
