63aa6890928a2d4853208da665e12dff5181a113
ODUME VINCENT TUOYO <odumevincent19@gmail.com>
2025-03-13 12:49:25 +0000

    Update telegram_bot.py

--- backend/telegram_bot.py
+++ backend/telegram_bot.py
@@ -158,15 +158,11 @@ def start_command(message: Message):
                     message.chat.id, photo=welcome_photo,
                     # caption=f"Welcome back, {username}!\nPerform tasks and earn coins!",
                     caption=f"""
-                Hey, {username}!👋 Welcome to BoredTap!\
-                Tap, complete tasks, and stack up your coins!\
+                Hey, {username}!👋 Welcome to BoredTap!\nTap, complete tasks, and stack up your coins!
                 
-                BoredTap is a fun and rewarding platform where users earn \
-                coins by engaging with the app’s features. The more you \
-                tap, the more you earn--simple!\
+                BoredTap is a fun and rewarding platform where users earn coins by engaging with the app’s features. \nThe more you tap, the more you earn--simple!
                 
-                Invite your friends, family & colleagues to join the game! \
-                More taps = More coins = More rewards! 🚀🔥.
+                Invite your friends, family & colleagues to join the game!\nMore taps = More coins = More rewards! 🚀🔥.
                 """,
                     reply_markup=inline_keyboard
                 )
